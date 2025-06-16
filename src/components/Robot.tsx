2f5693c0cff5de4e4ffaee7b0da163af8ea45a1c
ghapzh <gabriel.alejandro.paparello@gmail.com>
2025-06-16 23:53:31 +0200

    portfolio

---
+++ src/components/Robot.tsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+import React from 'react';
+export default function App() {
+  return (
+      <Spline   style={{position: 'absolute', right: -153, overflow: 'hidden', top: -10, width:'600px',}} scene="https://prod.spline.design/32S9hWf8NszWecqh/scene.splinecode" />
+  );
+}
